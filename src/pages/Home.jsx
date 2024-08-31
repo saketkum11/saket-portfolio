40b13352e0ae83da44fc116201db9fa9c40b4662
saket kumar <kumarsaket601@gmail.com>
2024-08-31 21:59:23 +0530

    updated resume  link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
             <div className="flex flex-col gap-8 text-white border border-solid border-zinc-700 py-3 rounded-xl px-5 sm:text-sm">
               <Link
                 className="text-center text-cyan-600 font-bold"
-                to="https://drive.google.com/file/d/1rWhAkDTt0viuT2g9oCCtxprNkqFe8Jic/view?usp=sharing"
+                to="https://drive.google.com/file/d/1U9L-Suxzk8dSLPEOg5DeNBZ0g1lEM0Ti/view?usp=sharing"
               >
                 Resume Link
               </Link>
